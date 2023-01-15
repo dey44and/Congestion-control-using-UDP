8834f6edbfa33668ef85159d8db33c14f31d19cd
andreiiosif <andrei-iosif.ioja@student.tuiasi.ro>
2023-01-15 19:19:05 +0200

    v0.1 - QT Design improvements

--- Client/gui.py
+++ Client/gui.py
@@ -1,142 +1,197 @@
-# -*- coding: utf-8 -*-
-
 from PySide2.QtCore import (QCoreApplication, QMetaObject, QRect)
 from PySide2.QtGui import (QFont)
 from PySide2.QtWidgets import *
 from functions import *
 from client import *
-import os
+
 
 info = "Lista comenzi\n" \
        "ls - Afisare fisiere\n" \
        "add [file_name] - Adauga fisier\n" \
        "rm [file_name] - Sterge fisier\n"
 
+
 class Ui_MainWindow(object):
     def __init__(self):
         # Connecting controls to functions
         self.connected_to_server = False
 
-    def setupUi(self, MainWindow):
-        if MainWindow.objectName():
-            MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(587, 635)
-        self.centralwidget = QWidget(MainWindow)
+        # Widgets
+        self.centralwidget = None
+        self.server_connection_frame = None
+        self.connect_button = None
+        self.dest_ip_label = None
+        self.port_source_label = None
+        self.port_dest_label = None
+        self.input_ip = None
+        self.input_sport = None
+        self.input_dport = None
+        self.server_connection_title = None
+        self.server_connection_frame = None
+        self.user_conn_title = None
+        self.user_conn_frame = None
+        self.input_username = None
+        self.username = None
+        self.password = None
+        self.input_password = None
+        self.frame = None
+        self.command_line = None
+        self.send_request_title = None
+        self.command_line_label = None
+        self.submit = None
+        self.display_instr = None
+        self.textEdit = None
+        self.statusbar = None
+
+    def setup_ui(self, main_window):
+        if main_window.objectName():
+            main_window.setObjectName(u"MainWindow")
+
+        main_window.resize(587, 635)
+        self.centralwidget = QWidget(main_window)
         self.centralwidget.setObjectName(u"centralwidget")
+
         self.server_connection_frame = QFrame(self.centralwidget)
         self.server_connection_frame.setObjectName(u"server_connection_frame")
         self.server_connection_frame.setGeometry(QRect(10, 30, 281, 171))
+
         font = QFont()
         font.setStrikeOut(False)
         self.server_connection_frame.setFont(font)
         self.server_connection_frame.setAutoFillBackground(False)
         self.server_connection_frame.setFrameShape(QFrame.Panel)
         self.server_connection_frame.setFrameShadow(QFrame.Raised)
+
         self.connect_button = QPushButton(self.server_connection_frame)
         self.connect_button.setObjectName(u"connect_button")
         self.connect_button.setGeometry(QRect(80, 130, 93, 28))
         # Connection button command
         self.connect_button.clicked.connect(self.connect_to_server)
+
         self.dest_ip_label = QLabel(self.server_connection_frame)
         self.dest_ip_label.setObjectName(u"dest_ip_label")
         self.dest_ip_label.setGeometry(QRect(10, 40, 71, 16))
+
         font1 = QFont()
         font1.setPointSize(10)
         font1.setStrikeOut(False)
+
         self.dest_ip_label.setFont(font1)
         self.port_source_label = QLabel(self.server_connection_frame)
         self.port_source_label.setObjectName(u"port_source_label")
         self.port_source_label.setGeometry(QRect(10, 70, 111, 21))
         self.port_source_label.setFont(font1)
+
         self.port_dest_label = QLabel(self.server_connection_frame)
         self.port_dest_label.setObjectName(u"port_dest_label")
         self.port_dest_label.setGeometry(QRect(10, 100, 91, 21))
         self.port_dest_label.setFont(font1)
+
         self.input_ip = QLineEdit(self.server_connection_frame)
         self.input_ip.setObjectName(u"input_ip")
         self.input_ip.setGeometry(QRect(110, 40, 151, 22))
+
         self.input_sport = QLineEdit(self.server_connection_frame)
         self.input_sport.setObjectName(u"input_sport")
         self.input_sport.setGeometry(QRect(110, 70, 151, 22))
+
         self.input_dport = QLineEdit(self.server_connection_frame)
         self.input_dport.setObjectName(u"input_dport")
         self.input_dport.setGeometry(QRect(110, 100, 151, 22))
+
         self.server_connection_title = QLabel(self.server_connection_frame)
         self.server_connection_title.setObjectName(u"server_connection_title")
         self.server_connection_title.setGeometry(QRect(30, 10, 191, 16))
+
         font2 = QFont()
         font2.setPointSize(12)
         font2.setStrikeOut(False)
+
         self.server_connection_title.setFont(font2)
         self.user_conn_frame = QFrame(self.centralwidget)
         self.user_conn_frame.setObjectName(u"user_conn_frame")
         self.user_conn_frame.setGeometry(QRect(300, 30, 281, 111))
         self.user_conn_frame.setFrameShape(QFrame.Panel)
         self.user_conn_frame.setFrameShadow(QFrame.Raised)
+
         self.username = QLabel(self.user_conn_frame)
         self.username.setObjectName(u"username")
         self.username.setGeometry(QRect(10, 40, 101, 16))
+
         font3 = QFont()
         font3.setPointSize(10)
         self.username.setFont(font3)
+
         self.password = QLabel(self.user_conn_frame)
         self.password.setObjectName(u"password")
         self.password.setGeometry(QRect(10, 70, 111, 21))
         self.password.setFont(font3)
+
         self.input_username = QLineEdit(self.user_conn_frame)
         self.input_username.setObjectName(u"input_username")
         self.input_username.setGeometry(QRect(110, 40, 151, 22))
+
         self.input_password = QLineEdit(self.user_conn_frame)
         self.input_password.setObjectName(u"input_password")
         self.input_password.setGeometry(QRect(110, 70, 151, 22))
+
         self.user_conn_title = QLabel(self.user_conn_frame)
         self.user_conn_title.setObjectName(u"user_conn_title")
         self.user_conn_title.setGeometry(QRect(10, 0, 181, 31))
+
         font4 = QFont()
         font4.setPointSize(12)
         self.user_conn_title.setFont(font4)
+
         self.frame = QFrame(self.centralwidget)
         self.frame.setObjectName(u"frame")
         self.frame.setGeometry(QRect(10, 260, 571, 351))
         self.frame.setFrameShape(QFrame.Panel)
         self.frame.setFrameShadow(QFrame.Raised)
+
         self.command_line = QLineEdit(self.frame)
         self.command_line.setObjectName(u"command_line")
         self.command_line.setGeometry(QRect(160, 40, 241, 22))
+
         self.send_request_title = QLabel(self.frame)
         self.send_request_title.setObjectName(u"send_request_title")
         self.send_request_title.setGeometry(QRect(210, 10, 141, 21))
         self.send_request_title.setFont(font4)
+
         self.command_line_label = QLabel(self.frame)
         self.command_line_label.setObjectName(u"command_line_label")
         self.command_line_label.setGeometry(QRect(30, 40, 111, 21))
         self.command_line_label.setFont(font3)
+
         self.submit = QPushButton(self.frame)
         self.submit.setObjectName(u"submit")
         self.submit.setGeometry(QRect(432, 40, 131, 28))
         # Submit button command
         self.submit.clicked.connect(self.send_request)
+
         self.display_instr = QPushButton(self.frame)
         self.display_instr.setObjectName(u"display_instr")
         self.display_instr.setGeometry(QRect(430, 80, 131, 28))
         # Add display instruction button
         self.display_instr.clicked.connect(self.display_instructions)
+
         self.textEdit = QTextEdit(self.frame)
         self.textEdit.setObjectName(u"textEdit")
         self.textEdit.setGeometry(QRect(160, 120, 241, 221))
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.statusbar = QStatusBar(MainWindow)
+
+        main_window.setCentralWidget(self.centralwidget)
+        self.statusbar = QStatusBar(main_window)
         self.statusbar.setObjectName(u"statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+        main_window.setStatusBar(self.statusbar)
 
-        self.retranslateUi(MainWindow)
+        self.retranslate_ui(main_window)
 
-        QMetaObject.connectSlotsByName(MainWindow)
+        QMetaObject.connectSlotsByName(main_window)
     # setupUi
 
     # retranslateUi
-    def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+    def retranslate_ui(self, main_window):
+        main_window.setWindowTitle(QCoreApplication.translate("MainWindow", "MainWindow", None))
         self.connect_button.setText(QCoreApplication.translate("MainWindow", u"Connect", None))
         self.dest_ip_label.setText(QCoreApplication.translate("MainWindow", u"IP Dest:", None))
         self.port_source_label.setText(QCoreApplication.translate("MainWindow", u"Port Source:", None))
@@ -176,9 +231,6 @@ class Ui_MainWindow(object):
     def send_request(self):
         command = self.command_line.text()
 
-
-
     def display_instructions(self):
         self.textEdit.clear()
         self.textEdit.append(info)
-
